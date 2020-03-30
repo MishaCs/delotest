e3c81e18813ab58e078ed270f0334d644c30268a
MishaCs <42771067+MishaCs@users.noreply.github.com>
2020-03-30 10:41:21 +0300

    Add test

---
+++ app/js/main.js
@@ -0,0 +1,12 @@
+$("#example_id").ionRangeSlider({
+  skin: "square",
+  values: ['Не владею', 'Использую готовые решения', 'Использую готовые решения и умею их переделывать', 'Пишу сложный JS с нуля'],
+  keyboard: true,
+  grid: true,
+  grid_margin: true,
+  grid_snap: true,
+  hide_min_max: true,
+  hide_from_to: true,
+  from: 2,
+
+});
